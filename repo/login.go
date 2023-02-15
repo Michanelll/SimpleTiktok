f60a432a9b273fdedd689e848fa7daf397936b55
麦扣 <105119385+Michanelll@users.noreply.github.com>
2023-02-15 17:08:30 +0800

    登录注册dao层

--- repo/login.go
+++ repo/login.go
@@ -1,6 +1,9 @@
 package repo
 
-import "errors"
+import (
+	"errors"
+	"sync"
+)
 
 type Login struct {
 	Id         int64 `gorm:"primary_key"`
@@ -9,23 +12,38 @@ type Login struct {
 	Password   string `gorm:"size:100;notnull"`
 }
 
+type LoginDAO struct {
+}
+
+var (
+	loginDao  *LoginDAO
+	loginOnce sync.Once
+)
+
+func NewUserLoginDao() *LoginDAO {
+	loginOnce.Do(func() {
+		loginDao = new(LoginDAO)
+	})
+	return loginDao
+}
+
 //账号密码查询
-func QueryUserLogin(username, password string, login *Login) error {
+func (u *LoginDAO) QueryUserLogin(username, password string, login *Login) error {
 	if login == nil {
-		return errors.New("Err:结构体空指针")
+		return errors.New("空指针错误")
 	}
 	Db.Where("username=? and password=?", username, password).First(login)
 	if login.Id == 0 {
-		return errors.New("Err:账号或密码出错")
+		return errors.New("用户不存在，账号或密码出错")
 	}
 	return nil
 }
 
 //账号查询
-func IsUserExistByUsername(username string) bool {
-	var userLogin Login
-	Db.Where("username=?", username).First(&userLogin)
-	if userLogin.Id == 0 {
+func (u *LoginDAO) IsUserExistByUsername(username string) bool {
+	var login Login
+	Db.Where("username=?", username).First(&login)
+	if login.Id == 0 {
 		return false
 	}
 	return true
--- repo/user.go
+++ repo/user.go
@@ -1,5 +1,10 @@
 package repo
 
+import (
+	"errors"
+	"sync"
+)
+
 type User struct {
 	Id            int64      `json:"id" gorm:"id,omitempty"`
 	Name          string     `json:"name" gorm:"name,omitempty"`
@@ -11,4 +16,36 @@ type User struct {
 	Follows       []*User    `json:"-" gorm:"many2many:user_relations;"`    //用户之间的多对多
 	FavorVideos   []*Video   `json:"-" gorm:"many2many:user_favor_videos;"` //用户与点赞视频之间的多对多
 	Comments      []*Comment `json:"-"`                                     //用户与评论的一对多
+}
+type UserDao struct {
+}
+
+var (
+	userDao  *UserDao
+	userOnce sync.Once
+)
+
+func NewUserDao() *UserDao {
+	userOnce.Do(func() {
+		userDao = new(UserDao)
+	})
+	return userDao
+}
+func (u *UserDao) QueryUserInfoById(userId int64, user *User) error {
+	if user == nil {
+		return errors.New("空指针错误")
+	}
+	Db.Where("id=?", userId).First(user)
+	//Db.Where("id=?", userId).Select([]string{"id", "name", "follow_count", "follower_count", "is_follow"}).First(user)
+	if user.Id == 0 {
+		return errors.New("该用户不存在")
+	}
+	return nil
+}
+
+func (u *UserDao) AddUserInfo(user *User) error {
+	if user == nil {
+		return errors.New("空指针错误")
+	}
+	return Db.Create(user).Error
 }
